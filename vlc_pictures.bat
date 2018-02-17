a8f2819c10e78b72b79aa5e028fc46424b93d37e
xachman <zironside@hotmail.com>
2018-02-17 20:57:40 +0000

    add very slow rate

--- vlc_pictures.bat
+++ vlc_pictures.bat
@@ -3,4 +3,4 @@
 Taskkill /IM vlc.exe
 
 
-START vlc.exe --fullscreen --qt-fullscreen-screennumber=3 --no-video-title-show --run-time=3600  %1
+START vlc.exe --fullscreen --qt-fullscreen-screennumber=3 --no-video-title-show --rate=0.000000000000001 --run-time=3600 %1
